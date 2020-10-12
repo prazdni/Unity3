dcc62177318c7f63a05a39da2dfd724d407d22da
Danil <danilstasilevich@yandex.ru>
2020-10-12 17:07:09 +0700

    Lesson6 Scripts
    Added main Lesson 6 scripts

---
+++ Controller/DataController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyLabyrinth
+{
+    public class SavedDataController
+    {
+        private readonly SaveDataRepository _saveDataRepository;
+
+        private SaveDataButton _savedData;
+        private LoadDataButton _loadedData;
+        
+        public SavedDataController()
+        {
+            _saveDataRepository = new SaveDataRepository();
+            Object.FindObjectOfType<SaveDataButton>().ButtonAction += SaveData;
+            Object.FindObjectOfType<LoadDataButton>().ButtonAction += LoadData;
+
+        }
+
+        private void SaveData()
+        {
+            GameObject[] allSceneObjects;
+            allSceneObjects = Object.FindObjectsOfType<GameObject>();
+            Debug.Log(allSceneObjects.Length);
+            //_saveDataRepository.Save();
+        }
+
+        private void LoadData()
+        {
+            //_saveDataRepository.Load
+        }
+        
+    }
+}
